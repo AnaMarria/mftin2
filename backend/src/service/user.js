d32bc09bac1ba46146da76266215d13ab9fa9951
Maria <a.marria93@yahoo.com>
2017-11-16 19:17:49 +0200

    update user rest api with delete method

--- backend/src/service/user.js
+++ backend/src/service/user.js
@@ -14,7 +14,29 @@ exports.create = function (req, res) {
 
 exports.findById = function (req, res) {
   let id = req.params.id;
-  user.findById(id).then(user => {
-    res.jsonp(user);
-  });
+  return user.findById(id).then(user => {
+    if (!user) {
+      return res.status(400).send({
+        message: 'User Not Found',
+      });
+    }
+    return res.jsonp(user);
+  }).catch(error => res.status(400).send(error));
 };
+
+exports.delete = function (req, res) {
+  return user
+    .findById(req.params.id)
+    .then(usertobedeleted => {
+      if (!usertobedeleted) {
+        return res.status(400).send({
+          message: 'User Not Found',
+        });
+      }
+      return usertobedeleted
+        .destroy()
+        .then(() => res.status(204).send())
+        .catch(error => res.status(400).send(error));
+    })
+    .catch(error => res.status(400).send(error));
+}
