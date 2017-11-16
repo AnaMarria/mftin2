2f8d995fe1f1fa3867d2f8f96d25f4c50aa8769e
roxanaaa1 <roxanaavadani@gmail.com>
2017-11-16 18:38:48 +0200

    Recipe rest working

--- backend/src/rest/recipe.js
+++ backend/src/rest/recipe.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 var recipe = require('../service/').recipe;
 
-router.get('/', recipe.list);
-router.get('/:id', recipe.findById);
-router.post('/', recipe.create);
+router.get('/recipe/', recipe.list);
+router.get('/recipe/:id', recipe.findById);
+router.post('/createrecipe', recipe.create);
 
+router.get('/recipe', (req, res) => res.status(200).send({
+    message: recipe
+ }));
+  
+router.post('/createrecipe', (req, res) => res.status(200).send({
+    message: recipe.create
+}));
 module.exports = router;
