2b1dc8be4fc40cd3b2ff9995824cb3674f8753a8
roxanaaa1 <roxanaavadani@gmail.com>
2017-11-16 18:42:25 +0200

    Url recipe changed

--- backend/src/rest/recipe.js
+++ backend/src/rest/recipe.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 var recipe = require('../service/').recipe;
 
-router.get('/recipe/', recipe.list);
-router.get('/recipe/:id', recipe.findById);
-router.post('/createrecipe', recipe.create);
+router.get('/', recipe.list);
+router.get('/:id', recipe.findById);
+router.post('/', recipe.create);
 
 router.get('/recipe', (req, res) => res.status(200).send({
     message: recipe
