e029783ecc05eb2eee7b2a072dd2ea7583e95c35
Maria <a.marria93@yahoo.com>
2017-12-21 07:41:50 +0200

    update FE user component

--- frontend/src/app/components/pages/user/user.component.ts
+++ frontend/src/app/components/pages/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../service/';
+import { MenuItem } from 'primeng/components/common/menuitem';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.less']
 })
 export class UserComponent implements OnInit {
+  users: User[];
+  selectedUser: User;
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.get('api/user/').subscribe(res => {
+      this.users = res;
+    });
   }
+  deleteUser(user: User): void {
+    this.apiService
+      .delete(user)
+      .then(() => {
+        this.users = this.users.filter(h => h !== user);
+        if (this.selectedUser === user) { this.selectedUser = null; }
+      });
+  }
+}
 
+interface User {
+  id: number;
+  username: string;
+  password: string;
+  email: string;
 }
