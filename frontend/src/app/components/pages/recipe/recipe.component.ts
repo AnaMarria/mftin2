90159569e690cf62591402bd0cc40119733c0fa3
roxanaaa1 <roxanaavadani@gmail.com>
2018-01-04 21:46:31 +0200

    Added recipe table

--- frontend/src/app/components/pages/recipe/recipe.component.ts
+++ frontend/src/app/components/pages/recipe/recipe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../service/';
+import { MenuItem } from 'primeng/components/common/menuitem';
 
 @Component({
   selector: 'app-recipe',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[];
+  selectedRecipe: Recipe;
+  items: MenuItem[];
 
-  ngOnInit() {
+  constructor(private apiService: ApiService) { }
+
+    ngOnInit() {
+      this.apiService.get('api/recipe/').subscribe(res => {
+        this.recipes = res;
+      });
+
+      this.items = [
+        { label: 'View', icon: 'fa-search', command: (event) => this.viewRecipe(this.selectedRecipe) },
+        { label: 'Delete', icon: 'fa-close', command: (event) => this.deleteRecipe(this.selectedRecipe) }
+      ];
+    }
+      viewRecipe(select: Recipe) {
+        console.log(JSON.stringify(select));
+
+      }
+      deleteRecipe(select: Recipe) {
+        this.apiService.delete('api/recipe/' + select.id).subscribe(res => {
+          console.log(res);
+        });
+    }
   }
 
+export interface Recipe {
+  id;
+  title;
+  description;
+  rating;
+  duration;
+  difficulty;
+  photo;
+  videoLink;
 }
