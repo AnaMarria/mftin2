4328698977294b65b372ffc99d2235393b173563
roxanaaa1 <roxanaavadani@gmail.com>
2018-01-11 19:29:55 +0200

    not final commit

--- frontend/src/app/components/pages/user/user.component.ts
+++ frontend/src/app/components/pages/user/user.component.ts
@@ -12,40 +12,52 @@ export class UserComponent implements OnInit {
   users: User[];
   selectedUser: User;
   items: MenuItem[];
-
+  newUser:User = new User('','','');
   constructor(private apiService: ApiService) { }
 
-    ngOnInit() {
-      this.apiService.get('api/user/').subscribe(res => {
-        this.users = res;
-      });
+  ngOnInit() {
 
-      this.items = [
-        { label: 'View', icon: 'fa-search', command: (event) => this.viewRecipe(this.selectedUser) },
-        { label: 'Delete', icon: 'fa-close', command: (event) => this.deleteRecipe(this.selectedUser) },
-        { label: 'Edit', icon: 'fa-edit', command: (event) => this.editRecipe(this.selectedUser) }
-      ];
-    }
-      viewRecipe(select: User) {
-        alert('Ai accesat userul cu numele: ' + select.username + ' si adresade email ' + select.email);
-        console.log(select);
-      }
-      editRecipe(select: User) {
-        console.log(JSON.stringify(select));
+    this.apiService.get('api/user/').subscribe(res => {
+      this.users = res;
+    });
 
-      }
-      deleteRecipe(select: User) {
-        alert('Utilizatorul ' + select.username + ' a fost sters, please refresh the page!');
+    this.items = [
+      { label: 'View2', icon: 'fa-search', command: (event) => this.viewRecipe2(this.selectedUser) },
 
-        this.apiService.delete('api/user/' + select.id).subscribe(res => {
-          console.log(res);
-        });
-    }
+      { label: 'View', icon: 'fa-search', command: (event) => this.viewRecipe(this.selectedUser) },
+      { label: 'Delete', icon: 'fa-close', command: (event) => this.deleteRecipe(this.selectedUser) },
+      { label: 'Edit', icon: 'fa-edit', command: (event) => this.editRecipe(this.selectedUser) }
+    ];
+  }
+  viewRecipe2(select: User) {
+     this.apiService.post('api/user',this.newUser).subscribe();
+
+  }
+  viewRecipe(select: User) {
+    alert('Ai accesat userul cu numele: ' + select.username + ' si adresade email ' + select.email);
+    console.log(select);
+  }
+  editRecipe(select: User) {
+    console.log(JSON.stringify(select));
+
+  }
+  deleteRecipe(select: User) {
+    alert('Utilizatorul ' + select.username + ' a fost sters, please refresh the page!');
+
+    this.apiService.delete('api/user/' + select.id).subscribe(res => {
+      console.log(res);
+    });
+  }
 }
 
-interface User {
+class User {
   id: number;
   username: string;
   password: string;
   email: string;
+  constructor( username?: string, password?: string, email?: string) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
 }
