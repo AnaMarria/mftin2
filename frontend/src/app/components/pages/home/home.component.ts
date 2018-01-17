8bd752b793470d9e6129d2071478d989ebcc9f73
Maria <a.marria93@yahoo.com>
2018-01-17 23:48:06 +0200

    home and user update

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -7,35 +7,8 @@ import { MenuItem } from 'primeng/components/common/menuitem';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-  users: User[];
-  selectedUser: User;
-  items: MenuItem[];
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
-    this.apiService.get('api/user/').subscribe(res => {
-      this.users = res;
-    });
-    this.items = [
-      { label: 'View', icon: 'fa-search', command: (event) => this.viewUser(this.selectedUser) },
-      { label: 'Delete', icon: 'fa-close', command: (event) => this.deleteUser(this.selectedUser) }
-    ];
   }
-  viewUser(select: User) {
-    console.log(JSON.stringify(select));
-
-  }
-  deleteUser(select: User) {
-    this.apiService.delete('api/user/' + select.id).subscribe(res => {
-      console.log(res);
-    });
-
-  }
-}
-
-interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
 }
